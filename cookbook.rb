874c4b1bd2f9ed8bde637df1624ed97b294f8834
Luana Santos <santosmeiraluana@gmail.com>
2022-07-28 20:54:03 -0300

    arrays e laços repetição

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,28 @@
 puts "Bem vindo ao CookBook, sua rede social de receitas"
 
-puts "Digite o nome da receita: "
-nome = gets.chomp()
+receitas = []
 
-puts "Receita #{nome} cadastrada com sucesso!"
+while(true) do
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
+
+    receitas << nome
+
+    puts
+    puts "Receita #{nome} cadastrada com sucesso!"
+    puts
+    puts "======== Receitas cadastradas ========"
+
+    puts receitas
+    
+    #receitas.each do |receita|
+    #    puts receita
+    #end
+
+    #aqui é um for em ruby
+    #for receita in receitas do
+    #    puts receita
+    #end
+
+    puts
+end
