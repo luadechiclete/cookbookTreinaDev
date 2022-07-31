da4083639ce71bacd8053f93fd74bee70210642a
Luana Santos <santosmeiraluana@gmail.com>
2022-07-31 08:55:48 -0300

    menu de opções, condições e hash

--- cookbook.rb
+++ cookbook.rb
@@ -2,27 +2,37 @@ puts "Bem vindo ao CookBook, sua rede social de receitas"
 
 receitas = []
 
-while(true) do
-    puts "Digite o nome da receita: "
-    nome = gets.chomp()
+puts "[1] Cadastrar uma Receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+print "Escolha uma opção: "
+opcao = gets.to_i()
 
-    receitas << nome
+while(opcao != 3) do
 
-    puts
-    puts "Receita #{nome} cadastrada com sucesso!"
-    puts
-    puts "======== Receitas cadastradas ========"
+    if(opcao == 1)
+        puts "Digite o nome da receita: "
+        nome = gets.chomp()
+        puts "Digite o tipo da receita: "
+        tipo = gets.chomp()
 
-    puts receitas
-    
-    #receitas.each do |receita|
-    #    puts receita
-    #end
-
-    #aqui é um for em ruby
-    #for receita in receitas do
-    #    puts receita
-    #end
-
-    puts
+        receitas << { nome: nome, tipo: tipo }
+        puts
+        puts "Receita #{nome} cadastrada com sucesso!"
+        puts
+    elsif(opcao == 2)
+        puts "======== Receitas cadastradas ========"
+        receitas.each do |receita|
+            puts "#{receita[:nome]} - #{receita[:tipo]}"
+        end
+    else
+        puts "Opção inválida"
+    end
+    puts "[1] Cadastrar uma Receita"
+    puts "[2] Ver todas as receitas"
+    puts "[3] Sair"
+    print "Escolha uma opção: "
+    opcao = gets.to_i()
 end
+
+puts 'Obrigado por usar o Cookbook, até logo!'
